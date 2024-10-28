4318b94fb501eb8862b4c9428f8fc84eaa59b516
Rysaev Damir <freepad@ya.ru>
2024-10-28 15:47:15 +0300

    axios example

--- test/auth.test.js
+++ test/auth.test.js
@@ -1,23 +1,17 @@
 import { config as _config } from '../framework'
+import axios from 'axios'
 
 const config = _config.dummyjson
 
 describe('Auth', () => {
   it('Success login', async () => {
-    const response = await fetch(`${config.baseURL}/auth/login`, {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({
-        username: config.username,
-        password: config.password,
-        expiresInMins: 30
-      })
+    const response = await axios.post(`${config.baseURL}/auth/login`, {
+      username: config.username,
+      password: config.password
     })
-    const data = await response.json()
-
     expect(response.status).toEqual(200)
-    expect(data.username).toBe(config.username)
-    expect(data.accessToken).toBeTruthy()
+    expect(response.data.username).toBe(config.username)
+    expect(response.data.accessToken).toBeTruthy()
   })
 
   it('Failed login', async () => {
